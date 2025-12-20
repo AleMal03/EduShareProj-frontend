f5d9790c4629d585182e586a9822fe29ba0a28f3
Alessandro Mallardi <alessandrom2003@gmail.com>
2025-12-20 18:40:10 +0100

    Revert "Tolto data (perché prendiamo i dati dal backend) e modificato dataModel con interfacce passate dal backend"
    This reverts commit f9bcecb67238c362288237a03df6bfd778b5e881.

--- src/data/data-model.ts
+++ src/data/data-model.ts
@@ -1,38 +1,198 @@
 export interface User{
-    name: string;
-	surname: string;
-	username: string;
-	email: string;
-    age?: number;
-    nationality?: string;
-    spokenLanguages: string[];
+    id?: number;
+    name? : string;
+    surname? : string;
+    username : string;
+    email: string;
+    age : number;
+    nationality: string;
+    spoken_languages : string[];
+    corsi: Corso[];
+}
+
+export class UsersManagment{
+
+    private users : User[];
+
+    constructor(){
+        this.users = [];
+    }
+
+    addNewUser(name: string, surname: string, username: string, email: string, age: number, nationality: string, spoken_languages: string[], corsi: Corso[]): boolean{
+        if(this.users.find(u => u.email === email))
+            return false;
+
+        const newId = this.users.length + 1;
+
+        // 3. Creo il nuovo oggetto User
+        const newUser: User = {
+            id: newId,
+            name: name,          
+            surname: surname,    
+            username: username,
+            email: email,
+            age: age,
+            nationality: nationality,
+            spoken_languages: spoken_languages,
+            corsi: corsi
+        };
+
+        this.users.push(newUser);
+        return true;
+    }
+
+    getAllUsers(){
+        return this.users.map(x => x);
+    }
+
+    getUserById(id: number): User | undefined{
+        return this.users.find(u => u.id === id);
+    } 
+
+    getUsersByName(name: string): User[]{
+        const filteredUsers : User[] = [];
+
+        for(const u of this.users){
+            if(u.name === name)
+                filteredUsers.push(u);
+        }
+
+        return filteredUsers;
+    }
+
+    getUsersByNameAndSurname(name: string, surname: string): User[]{
+        const filteredUsers : User[] = [];
+
+        for(const u of this.users){
+            if(u.name === name && u.surname === surname)
+                filteredUsers.push(u);
+        }
+
+        return filteredUsers;
+    }
+
+    getUsersByNationality(nationality: string): User[]{
+        const filteredUsers : User[] = [];
+
+        for(const u of this.users){
+            if(u.nationality === nationality)
+                filteredUsers.push(u);
+        }
+
+        return filteredUsers;
+    }
+
+    getUserByUsername(username: string): User | null{
+        for(const u of this.users){
+            if(u.username === username)
+                return u;
+        }
+
+        return null;
+    }
+
+    getAllCoursesByUsername(username: string): Corso[]{
+        for(let i of this.users){
+            if(i.username == username)
+                return i.corsi;
+        }
+
+       return [];
+    }
+
+    addNewCourse(username: string, nome_corso: string, nome_icona: string, difficolta: string): boolean {
+        const userTarget = this.users.find(u => u.username === username);
+
+        if (!userTarget) 
+            return false;
+
+        const nuovoCorso = new Corso(userTarget.corsi.length+1, nome_corso, nome_icona, difficolta);
+        userTarget.corsi.push(nuovoCorso);
+        return true;
+    }
+
+    removeCourse(username: string, index: number): boolean{
+        const userTarget = this.users.find(u => u.username === username);
+
+        if (!userTarget) 
+            return false;
+
+        userTarget.corsi.find(() => userTarget.corsi.splice(index-1, 1));
+        
+        for(let i of userTarget.corsi)
+            if(i.getIndex() >= index)
+                i.setIndex(i.getIndex() - 1);
+
+        return true;
+    }
+
+
 }
 
 
-// User <- {Student, Teacher}
-
-export interface Student extends User{
-	role: "STUDENT";	// Discriminante
+export interface Video{
+    titolo: string;
+    percorso: string;
 }
 
-export interface Teacher extends User{
-	role: "TEACHER";	// Discriminante
-	aboutMe: string;
-	titoliStudio: string[];
-}
 
-export interface File{
-	nome: string;
-    path: string;
-	icona: string;
-}
+export class Corso{
+    private index: number;
+    private nome: string;
+    private percorso_icona: string;
+    private difficolta: string;
+    private video: Video[];
 
-export interface Corso{
-    id: number;
-    nome: string;
-	prezzo: number;
-	materia: string;
-	difficolta: string;
-    icona: string;
-    files: File[];
+    constructor(index: number, nome: string, nome_icona: string, difficolta: string){
+        this.index = index;
+        this.nome = nome;
+        this.percorso_icona = "./public/miei_corsi/" + nome_icona;
+        this.difficolta = difficolta
+        this.video = [];
+    }
+
+    getIndex(){
+        return this.index;
+    }
+
+    setIndex(index: number){
+        this.index = index;
+    }
+
+    getNome(){
+        return this.nome;
+    }
+
+    getDifficolta(){
+        return this.difficolta;
+    }
+
+    getNumberOfVideos(){
+        return this.video.length;
+    }
+
+    getPercorsoIcona(){
+        return this.percorso_icona;
+    }
+
+    setDifficolta(difficolta: string){
+        this.difficolta = difficolta;
+    }
+
+    setNomeIcona(nome: string){
+        this.percorso_icona = this.percorso_icona + nome;
+    }
+
+    getAllVideo(){
+        return this.video.map((x) => x);
+    }
+
+    addNewVideo(video: Video){
+        this.video.push(video);
+    }
+
+    removeVideoByIndex(index: number){
+        this.video.splice(index, 1);
+    }
+
 }
---
+++ src/data/data.ts
@@ -0,0 +1,11 @@
+import { UsersManagment } from "./data-model"
+
+export const user_mgr = new UsersManagment();
+
+user_mgr.addNewUser("Simone", "Stridi", "Simo_prof", "simo@gmail.com", 20, "Italia", ["Italiano", "Inglese"], []);
+user_mgr.addNewUser("Chiara", "Rossi", "ch_crf1", "chiara@gmail.com", 34, "Francia", ["Francese", "Inglese", "Italiano"], []);
+user_mgr.addNewUser("Marco", "Grana", "marcooo", "marco@gmail.com", 27, "Italia", ["Italiano"], []);
+user_mgr.addNewUser("Giulia", "Marche", "giu", "giulia@gmail.com", 21, "Inghilterra", ["Inglese", "Italiano"], []);
+
+user_mgr.addNewCourse("Simo_prof", "DataBase", "DB.png", "facile");
+user_mgr.addNewCourse("Simo_prof", "Sistemi Operativi", "SO.png", "difficile");
