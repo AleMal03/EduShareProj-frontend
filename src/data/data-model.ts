d7f388953d7ec5be96e8187d6dd6d86037420c16
Alessandro Mallardi <alessandrom2003@gmail.com>
2025-12-20 18:34:36 +0100

    Corretto data model

--- src/data/data-model.ts
+++ src/data/data-model.ts
@@ -1,22 +1,20 @@
 interface GenericUser{
-    name: string;
+	name: string;
 	surname: string;
 	username: string;
 	email: string;
     age?: number;
     nationality?: string;
     spokenLanguages: string[];
+	ruoli: string[];
 }
 
 
 // User <- {Student, Teacher}
 
-export interface Student extends GenericUser{
-	role: "STUDENT";	// Discriminante
-}
+export type Student = GenericUser	// Student è espandibile
 
 export interface Teacher extends GenericUser{
-	role: "TEACHER";	// Discriminante
 	aboutMe: string;
 	titoliStudio: string[];
 }
