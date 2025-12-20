5c43e43949c840aac65463044ad4e177048e53cb
Alessandro Mallardi <alessandrom2003@gmail.com>
2025-12-20 16:53:35 +0100

    Tolto data (perché prendiamo i dati dal backend) e modificato dataModel con interfacce passate dal backend

--- src/data/data-model.ts
+++ src/data/data-model.ts
@@ -1,4 +1,4 @@
-export interface User{
+interface GenericUser{
     name: string;
 	surname: string;
 	username: string;
@@ -11,15 +11,17 @@ export interface User{
 
 // User <- {Student, Teacher}
 
-export interface Student extends User{
+export interface Student extends GenericUser{
 	role: "STUDENT";	// Discriminante
 }
 
-export interface Teacher extends User{
+export interface Teacher extends GenericUser{
 	role: "TEACHER";	// Discriminante
 	aboutMe: string;
 	titoliStudio: string[];
 }
+
+export type User = Student | Teacher;
 
 export interface File{
 	nome: string;
